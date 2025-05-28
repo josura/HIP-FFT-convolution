7dc6a7dba674801e90be1b89ac48c0b967a8f46c
josura <giorgiolocicero@hotmail.com>
2025-05-28 21:26:51 +0000

    fix: changed path for the hipfft library

--- include/fft_convolution.h
+++ include/fft_convolution.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <hip/hip_runtime.h>
-#include <hipfft.h>
+#include <hipfft/hipfft.h>
 
 hipError_t run_fft_convolution(float* input, float* filter, float* output,
                                 int signal_size, int filter_size, hipStream_t stream);
